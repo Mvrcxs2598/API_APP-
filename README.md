90ffb7c2977dc273f4f1fc3c51907f70309c38f7
Marcos <mibanez6@gatech.edu>
2026-01-14 16:19:08 -0600

    Setup for Railway

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 Using Railway as a public URL domain.
 
 Railway todo: 
-Railway pulls the code from github. 
-creates a runable image, and starts the web server. 
+
+Railway pulls the code from github.
+Creates a runable image, and starts the web server. 
 Has a postgres DB service. 
 Public URL: 
 
+DB_volume(local): apiget_pgdata
+DB: api_app
--- config/settings.py
+++ config/settings.py
@@ -11,13 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-cch8&i94w2kad9xo*y!pf3bzv(1v^t9=le)3ml!5p68k-#*5^d'
@@ -75,9 +74,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["PGDATABASE"],
+        "USER": os.environ["PGUSER"],
+        "PASSWORD": os.environ["PGPASSWORD"],
+        "HOST": os.environ["PGHOST"],
+        "PORT": os.environ.get("PGPORT", "5432"),
     }
 }
 
